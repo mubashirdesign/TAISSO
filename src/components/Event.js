5b58872eb126d12d1e1ffe11d9496454e55fdeee
unknown <you@example.com>
2022-05-19 23:48:06 -0400

    add padding

--- src/components/Event.js
+++ src/components/Event.js
@@ -37,7 +37,7 @@ export default function Event() {
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 mx-auto max-w-7xl lg:px-8 mt-10">
+      <div className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8 mx-auto max-w-7xl px-4 lg:px-8 mt-10">
         {files.map((file) => (
           <a href={file.source} key={file.source} className="relative">
             <div className="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
